d9b6fbedec77494b4ebcc9a1a71d11e39d6c894a
Libin <libinson@qusol.org>
2015-07-28 10:01:37 +0530

    Initializing

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/peoplelifecart/admin/');
+define('HTTP_CATALOG', 'http://localhost/peoplelifecart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/peoplelifecart/admin/');
+define('HTTPS_CATALOG', 'http://localhost/peoplelifecart/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/peoplelifecart/admin/');
+define('DIR_SYSTEM', '/var/www/html/peoplelifecart/system/');
+define('DIR_LANGUAGE', '/var/www/html/peoplelifecart/admin/language/');
+define('DIR_TEMPLATE', '/var/www/html/peoplelifecart/admin/view/template/');
+define('DIR_CONFIG', '/var/www/html/peoplelifecart/system/config/');
+define('DIR_IMAGE', '/var/www/html/peoplelifecart/image/');
+define('DIR_CACHE', '/var/www/html/peoplelifecart/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/peoplelifecart/system/download/');
+define('DIR_UPLOAD', '/var/www/html/peoplelifecart/system/upload/');
+define('DIR_LOGS', '/var/www/html/peoplelifecart/system/logs/');
+define('DIR_MODIFICATION', '/var/www/html/peoplelifecart/system/modification/');
+define('DIR_CATALOG', '/var/www/html/peoplelifecart/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'pass');
+define('DB_DATABASE', 'peoplelifecart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'plc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/peoplelifecart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/peoplelifecart/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/peoplelifecart/catalog/');
+define('DIR_SYSTEM', '/var/www/html/peoplelifecart/system/');
+define('DIR_LANGUAGE', '/var/www/html/peoplelifecart/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/html/peoplelifecart/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/html/peoplelifecart/system/config/');
+define('DIR_IMAGE', '/var/www/html/peoplelifecart/image/');
+define('DIR_CACHE', '/var/www/html/peoplelifecart/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/peoplelifecart/system/download/');
+define('DIR_UPLOAD', '/var/www/html/peoplelifecart/system/upload/');
+define('DIR_MODIFICATION', '/var/www/html/peoplelifecart/system/modification/');
+define('DIR_LOGS', '/var/www/html/peoplelifecart/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'pass');
+define('DB_DATABASE', 'peoplelifecart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'plc_');
